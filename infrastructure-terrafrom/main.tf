20b9c823b0fd73c88acf0d12003967d50eecfa3d
Dmytro Symonenko <symonenko@thedigital.gov.ua>
2022-05-28 12:57:12 +0700

    single_nat_gateway     = false

--- infrastructure-terrafrom/main.tf
+++ infrastructure-terrafrom/main.tf
@@ -13,7 +13,7 @@ module "vpc" {
   enable_dns_hostnames   = true
   enable_dns_support     = true
   enable_nat_gateway     = true
-  single_nat_gateway     = true
+  single_nat_gateway     = false
   one_nat_gateway_per_az = false
 
   tags = var.tags
